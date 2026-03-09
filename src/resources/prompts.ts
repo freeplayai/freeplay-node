aa6f90c78cd3739be70107e4a812bd3671082bbf
Aditya Pandey <aditya@freeplay.ai>
2026-03-09 16:10:50 -0600

    fix: Store the prepared messages

--- src/resources/prompts.ts
+++ src/resources/prompts.ts
@@ -490,7 +490,7 @@ export class BoundPrompt {
     if (llmFormatText) {
       return new FormattedPrompt<MessageType>(
         effectivePromptInfo,
-        this.messages as MessageType[],
+        prepared as MessageType[],
         undefined,
         llmFormatText,
         formattedToolSchema,
@@ -499,7 +499,7 @@ export class BoundPrompt {
     } else {
       return new FormattedPrompt<MessageType>(
         effectivePromptInfo,
-        this.messages as MessageType[],
+        prepared as MessageType[],
         llmFormat,
         undefined,
         formattedToolSchema,
